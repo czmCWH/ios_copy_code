abab173907b2d2cffd0036530c71a8c461ad9a13
czm <2451072587@qq.com>
2022-01-18 16:25:27 +0800

    add

--- swift/FixedWrite.swift
+++ swift/FixedWrite.swift
@@ -15,3 +15,16 @@ NotificationCenter.default.removeObserver(self)
         break
     }
 }
+
+
+// MARK: - ------------------ 布局代码 -----------------------
+{
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let height: CGFloat = 40.0
+        let width: CGFloat = UIScreen.main.bounds.width
+        let y: CGFloat = UIScreen.main.bounds.height / 2.0 - height / 2.0
+        redButton.frame = CGRect(x: 0, y: y, width: width, height: height)
+    }
+}
+
