827c25c5ba683fe7c4d78f8b6669e6862bb988af
czm <2451072587@qq.com>
2022-04-07 09:33:04 +0800

    add

--- swift/02-wheel/11-String字符串文本处理/02-正则+CharacterSet /01-CharacterSet的基本使用.swift
+++ swift/02-wheel/11-String字符串文本处理/02-正则+CharacterSet /01-CharacterSet的基本使用.swift
@@ -11,3 +11,22 @@ https://www.jianshu.com/p/572dd7659012
 
 NSCharacterSet字符集：
 https://www.jianshu.com/p/75cb42e4593c
+
+
+func test1(_ validate: String) {
+    var chSet = CharacterSet()
+    chSet.formUnion(.whitespacesAndNewlines)        // 空格和换行
+    chSet.formUnion(.punctuationCharacters)     // 标点符号，连接线
+    chSet.formUnion(.controlCharacters)     // 控制符的字符集
+    chSet.formUnion(.illegalCharacters)     // 不合规字符，没有在Unicode 3.2 标准中定义的字符
+//        chSet.formUnion(.symbols)     //    符号的字符集
+    chSet.formUnion(.nonBaseCharacters)     // 非基础的字符集
+    let otherSyb = CharacterSet(charactersIn: "@／：；（）¥「」＂、[]{}#%-*+=_\\|~＜＞$€^•'@#$%^&*()_+'\"")
+    chSet.formUnion(otherSyb)     //    //
+    
+//        let str = validate.trimmingCharacters(in: chSet as CharacterSet)
+    
+//        let str1 = (validate.components(separatedBy: chSet) as NSArray).componentsJoined(by: "")
+    
+    let str1 = validate.components(separatedBy: .symbols).joined()
+}
--- swift/02-wheel/11-String字符串文本处理/02-正则+CharacterSet /02-正则表达式的基本使用.swift
+++ swift/02-wheel/11-String字符串文本处理/02-正则+CharacterSet /02-正则表达式的基本使用.swift
@@ -6,3 +6,32 @@
 Swift 正则表达式完整教程：https://www.jianshu.com/p/c579d3692876
 
 swift5.0判断字符串中是否含有emoji表情的那些坑：https://www.jianshu.com/p/17f5dfce472d
+
+
+func test1() {
+    guard let validate = textView.text else { return }
+    
+    let regex = "[\\u4e00-\\u9fa50-9a-zA-Z]"
+//        let regex = "[\\p{Script=Han}0-9a-zA-Z]"
+    
+//        let regex = "\\p{Script=Han}"
+//        let regex = "[\\u4e00-\\u9fa5]"
+   
+
+    let RE = try? NSRegularExpression(pattern: regex, options: .caseInsensitive)
+    guard let matchs = RE?.matches(in: validate, options: [], range: NSRange(validate.startIndex..., in: validate)) else { return }
+
+    
+    var data:[String] = Array()
+    for item in matchs {
+        let string = (validate as NSString).substring(with: item.range)
+        data.append(string)
+    }
+    
+    print("===czm====", data)
+    
+    
+    //        let str = validate.trimmingCharacters(in: chSet)
+            
+    //        let str = (validate.components(separatedBy: chSet) as NSArray).componentsJoined(by: "")
+}
